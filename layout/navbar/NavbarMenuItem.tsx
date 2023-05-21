c2a7051283c34b8d334410c447ba38a391b526ac
manikantjha <being.manikantjha@gmail.com>
2023-05-21 21:47:45 +0530

    Fixed menu color in desktop veiw.

--- layout/navbar/NavbarMenuItem.tsx
+++ layout/navbar/NavbarMenuItem.tsx
@@ -16,7 +16,7 @@ export default function NavbarMenuItem(props: INavbarMenuItem) {
         href={props.objMenuItem.path}
         className={`block py-2 pl-3 pr-4 md:p-0 ${
           props.isActive
-            ? "text-white font-semibold bg-primary md:bg-transparent"
+            ? "text-white lg:text-primary font-semibold bg-primary md:bg-transparent"
             : "md:text-black text-gray-700"
         } md:hover:underline md:hover:underline-offset-4`}
         aria-current="page"
